d0ed86a5fc37a07d91e2559cda8c6ffafed7da7b
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-07-18 16:37:51 -0500

    Add migration of psychosomatic_aptitudes cip_statuses civil statuses ubigeos police situations

---
+++ database/migrations/2023_07_18_185051_create_sub_units_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_units', function (Blueprint $table) {
+            $table->id();
+            $table->stirng('unit_name',50);
+            $table->string('department',50);
+            $table->string('department_final',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_units');
+    }
+};
